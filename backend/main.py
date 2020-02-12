1a1225d1c4d965a416f27b6f0dd89a94d5a99b61
Alexandr Shchelov <chadoishere@gmail.com>
2020-02-12 18:39:08 +0100

    Set up 'static' assets

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 from routers import root
 
 app = FastAPI()
+
+app.mount("/s", StaticFiles(directory="../frontend/public/"), name="static")
 
 app.include_router(root.router)

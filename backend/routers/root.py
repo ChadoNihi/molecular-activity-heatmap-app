6b97fe8e421e5fea89b29811283163f8d4f2a4ba
Alexandr Shchelov <chadoishere@gmail.com>
2020-02-12 14:52:37 +0100

    Return index.html from root

--- backend/routers/root.py
+++ backend/routers/root.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+from starlette.responses import FileResponse
 
 router = APIRouter()
 
 
 @router.get("/")
 async def root():
-    return 'hello'
+	# FIXME: make work w/ `await`? see
+	# https://www.starlette.io/responses/#fileresponse
+    return FileResponse('../frontend/public/index.html', media_type='text/html')

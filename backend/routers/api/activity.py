0d568364212a43b7f51017271554f879afeb2ed3
Alexandr Shchelov <chadoishere@gmail.com>
2020-02-14 19:50:01 +0100

    Serve pchembl data

--- backend/routers/api/activity.py
+++ backend/routers/api/activity.py
@@ -1,9 +1,15 @@
+import json
 from fastapi import APIRouter
 from starlette.responses import FileResponse
 
+ACTIVITY_DATA_JSON = "data/pchembl_values.json"
+
 router = APIRouter()
+
+with open(ACTIVITY_DATA_JSON, "r") as f:
+    activity_data = json.load(f)
 
 
 @router.get("/api/activity")
 async def activity():
-	pass
+	return activity_data
